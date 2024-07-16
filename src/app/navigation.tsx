6c54b9e2a56cf31790c96a1e020bd08b5f7b9872
Monica Björk <m.bjork95@gmail.com>
2024-07-16 22:49:54 +0200

    made a moduel of navigation

---
+++ src/app/navigation.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { SanityDocument } from "next-sanity";
+import { sanityFetch } from "@/app/client";
+
+const EVENTS_QUERY_MENU = `*[
+    _type == "event"
+    && defined(slug.current)
+  ]{_id, name, slug, date}|order(date desc)`;
+
+
+
+export default async function Navigation() {
+    const events = await sanityFetch<SanityDocument[]>({query: EVENTS_QUERY_MENU});
+    
+    return (
+        <nav className="navigation">
+            <h1>FunKit</h1>
+            <h3>Meny</h3>
+            <ul>
+                <li><Link href="/">Hem</Link></li>
+                {events.map((event) => (
+                    <li key={event._id} >
+                        <Link href={`/events/${event.slug.current}`}>{event?.name}</Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )   
+}
