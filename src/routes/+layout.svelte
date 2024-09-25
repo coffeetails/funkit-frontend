c4f44202cc0dd084056de739624a2edf07cc7e6d
Monica Björk <m.bjork95@gmail.com>
2024-09-25 16:07:05 +0200

    small update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,8 @@
 	let title = "Funkit";
 	let description = "✨ Funkit är en idéell förening med syfte att föra samman människor genom ett gemensamt intresse av olika sorters popkultur, så som spel, anime, böcker, och massor av andra spännande intressen! Föreningens plan är att en gång om året kunna arrangera VänCon, Nordens nyaste och mysigaste konvent där målet är att alla ska trivas oavsett vem man är.  🎉💖🌟!";
 	let image = "https://cdn.discordapp.com/attachments/1274041105918328926/1287404782180368404/IMG_0956.png?ex=66f56122&is=66f40fa2&hm=79e3e28fb71731ee276c9393e845d5ce5c414c92bd7e9408fc99dd365e77a44f&";
+	let themeColor = "#f02525";
+	let largeThumbnail = false;
 </script>
 
 <svelte:head>
@@ -11,7 +13,7 @@
 	<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌸</text></svg>">
 	<title>{title}</title>
 	<meta name="description" content={description}>
-	<meta name="theme-color" content="#f02525">
+	<meta name="theme-color" content={themeColor}>
 
 	<!--	 Facebook	 -->
 	<meta property="og_site_name" content=“Example.com”>
@@ -22,7 +24,9 @@
 	<meta property="og:image" content={image}>
 
 	<!-- 	Twitter / X 	-->
-	<meta name="twitter:card" content="summary_large_image"> <!-- Toggle for thumbnail size -->
+	{#if largeThumbnail}
+		<meta name="twitter:card" content="summary_large_image">
+	{/if}
 	<meta property="twitter:domain" content="example.com">
 	<meta property="twitter:url" content="https://www.example.com{$page.url.pathname.toString()}">
 	<meta name="twitter:title" content="{title}">
