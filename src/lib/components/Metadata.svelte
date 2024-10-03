81d66fe189dfe88450c923d22c956e3c8bbcaaaf
Monica Björk <m.bjork95@gmail.com>
2024-10-03 21:39:20 +0200

    small fix in metadata, typo

--- src/lib/components/Metadata.svelte
+++ src/lib/components/Metadata.svelte
@@ -12,7 +12,7 @@
 
 <svelte:head>
     {#await getMetadata() then metadata}
-		{console.log(metadata)}
+		<!-- {console.log(metadata)} -->
 		<!-- 	Metadata	 -->
 		<link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌸</text></svg>">
 		<title>{metadata.name}</title>
@@ -35,9 +35,9 @@
 		<meta property="twitter:url" content="https://www.{metadata.domain}{$page.url.pathname.toString()}">
 		<meta name="twitter:title" content="{metadata.name}">
 		<meta name="twitter:description" content={metadata.description}>
-		<meta name="twitter:image" content={image}>
+		<meta name="twitter:image" content={urlFor(metadata.thumbnailImage).url()}>
 		{#if metadata.twittername}
-			<meta name="twitter:site" content="@username" />
+			<meta name="twitter:site" content={"@" + metadata.twittername} />
 		{/if}
     {/await}
 
