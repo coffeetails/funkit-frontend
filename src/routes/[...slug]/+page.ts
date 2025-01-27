9317469b77ae7e483c89638e4dcf84daef6c4d78
Monica Björk <m.bjork95@gmail.com>
2025-01-27 21:54:17 +0100

    updated [...slug] so it can also redirect social media links

--- src/routes/[...slug]/+page.ts
+++ src/routes/[...slug]/+page.ts
@@ -1,19 +1,25 @@
-import { getPage } from '$lib/utils/sanity';
-import { error } from '@sveltejs/kit';
+import { getPage, getSocialmedia } from '$lib/utils/sanity';
+import { error, redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 
 export const load = (async ({ params }) => {
-    // console.log("params", params);
-    
+
     const page = await getPage(params.slug);
 
-    if (page) {
-        // console.log("page load", page); 
-        return {
-            page
-        };
+    console.log("page", page);
+    
+    const data = await getSocialmedia();
+    
+    let i = 1;
+    for(let social of data.socialmedialinks) {
+        if(social.title.toLowerCase() == params.slug) {
+            redirect(302, social.link);
+        } else if(i == data.socialmedialinks.length) {
+            return { page }
+        }
+        i++;
     }
-
+    
     throw error(404, 'Page not found');
 }) satisfies PageLoad;
